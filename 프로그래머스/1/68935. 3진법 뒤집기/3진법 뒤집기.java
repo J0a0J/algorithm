a99987076d5ef9a96253e32fc16071dd6fc3dfe3
J0a0J <105842502+J0a0J@users.noreply.github.com>
2024-06-24 15:33:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.56 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,45 +1,7 @@
-import java.lang.Math;
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int nn) {
-        long n = nn;
+    public int solution(int n) {
         int answer = 0;
-        int num = 0;
-        // 새로 만들 배열의 자릿수 구하는 과정
-        for (int i = 0; i < 30; i++) {
-            if (n >= Math.pow(3,i)) {
-                num = i;
-            } else break;
-        }
-        // 3진법으로 만들기 위한 배열
-        int[] result = new int[num + 1];
-        int[] reverse = new int[num + 1];
-        // 3진법으로 만드는 과정
-        for (int i = 0; i < num + 1; i++) {
-            if (n >= Math.pow(3, num - i)) {
-                if (n >= Math.pow(3, num - i) * 2) {
-                    result[i] = 2;
-                    n -= Math.pow(3, num - i) * 2;
-                } else {
-                    result[i] = 1;
-                    n -= Math.pow(3, num - i);
-                }
-            } else {
-                result[i] = 0;
-            }
-        }
-        
-        // 10진법 변환
-        for (int i = num; i > -1; i--) {
-            answer += Math.pow(3, num) * result[i];
-            num--;
-            System.out.println(answer);
-        }
-        
-        System.out.println(Arrays.toString(result));
         System.out.println(n);
-        
         return answer;
     }
 }
