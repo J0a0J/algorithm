a1f863e98800f777ccbb31f189a3970f3c7e0943
J0a0J <105842502+J0a0J@users.noreply.github.com>
2025-01-27 23:08:15 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int age) {
+        StringBuilder answer = new StringBuilder();
+        
+        while (age > 0) {
+            answer.insert(0, (char) ('a' + age % 10));
+            age /= 10;
+        }
+        return answer.toString();
+    }
+}
