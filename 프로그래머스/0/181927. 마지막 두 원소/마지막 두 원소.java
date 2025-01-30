e7bdfd7d90d2cb3152bb34bf076e7536870dde24
J0a0J <105842502+J0a0J@users.noreply.github.com>
2025-01-31 01:49:58 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        int[] answer = Arrays.copyOf(num_list, len + 1);
+        
+        if (answer[len - 1] > answer[len - 2]) {
+            answer[len] = answer[len - 1] - answer[len - 2];
+        } else {
+            answer[len] = answer[len - 1] * 2;
+        }
+        
+        return answer;
+    }
+}
